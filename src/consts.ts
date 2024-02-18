8f25fa74d403ef2129b75961ad3f741d4eef97b2
YiMing Han <hanyiming1995@gmail.com>
2024-02-18 14:38:14 -0500

    update

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "YiMing's Site";
-export const SITE_DESCRIPTION = "My personal site";
+export const SITE_DESCRIPTION = "YiMing Han's personal site";
